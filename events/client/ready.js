5eea3acd3cb063c57473c24326a5834ef19c1a4e
ghazapratama <fghaza.pratama@gmail.com>
2021-11-22 17:56:10 +0700

    ok

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,5 +1,5 @@
 module.exports = (Discord, client, message) => {
-    console.log(`GemBot is Online!`);
+    console.log(`${client.user.username} is Online!`);
 
     client.user.setPresence({
         activity: { 
