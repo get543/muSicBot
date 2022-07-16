0deb04c32e03f448755a58a91ec70831775eb565
ghazapratama <fghaza.pratama@gmail.com>
2022-07-16 19:28:14 +0700

    display actual time server goes online

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const server = express();
+const d = new Date();
 
 server.all(`/`, (req, res) => {
   res.send(`Result: [OK].`);
@@ -7,7 +8,7 @@ server.all(`/`, (req, res) => {
 
 function keepAlive() {
   server.listen(3000, () => {
-    console.log("Server is now ready! | " + Date.now());
+    console.log(`Server is now ready! | ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`);
   });
 }
 
